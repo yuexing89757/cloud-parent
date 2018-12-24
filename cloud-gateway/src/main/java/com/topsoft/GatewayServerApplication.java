da8ad3619b6414bc4079c846a2bd55c2c75077ec
guokaige <kaige8312@163.com>
2018-12-24 16:42:02 +0800

    通过zuul实现api-gateway

--- cloud-gateway/src/main/java/com/topsoft/GatewayServerApplication.java
+++ cloud-gateway/src/main/java/com/topsoft/GatewayServerApplication.java
@@ -3,16 +3,14 @@ package com.topsoft;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * @author kevin
  */
 @SpringBootApplication
 @EnableEurekaClient
-@RestController
+@EnableZuulProxy
 public class GatewayServerApplication {
 
 
